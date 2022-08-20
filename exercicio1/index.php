81e91c0ce20c310e5af9af5bdcb68b3396d183df
Edmar Gomes <edmar.gomes@nyx.tc>
2022-08-20 17:22:23 -0300

    Adicionado o formulario para o exercicio 1.

---
+++ exercicio1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 1</title>
+</head>
+<body>
+    <h1>Exercicio 1</h1>
+    <form action="/exercicio1" method="post">
+        <div>
+            <label for="value1">Digite o primeiro valor: </label>
+            <input type="number" name="value1">
+        </div>
+        <div>
+            <label for="value2">Digite o segundo valor: </label>
+            <input type="number" name="value2">
+        </div>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
