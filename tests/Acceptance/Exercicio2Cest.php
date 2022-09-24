d14c17264f09d07384a81c506f34e7d04cb29732
Edmar Gomes <edmar.gomes@nyx.tc>
2022-09-24 16:32:30 -0300

    Criando o primeiro teste e inicializando codeception.

---
+++ tests/Acceptance/Exercicio2Cest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exercicio2Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
